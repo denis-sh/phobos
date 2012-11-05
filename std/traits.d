fc0959a9fc98de11b5ae08699352f36a47dee3e2
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-11-05 09:46:28 +0400

    Improve comments as @alexrp suggested

--- std/traits.d
+++ std/traits.d
@@ -4353,7 +4353,7 @@ unittest
 
 
 /**
-Get static array dimensions.
+Gets the rank (number of dimensions) of a static array type.
 
 Example:
 ---
@@ -4387,8 +4387,8 @@ unittest
 
 
 /**
-Get multidimensional static array element type considering 
-$(D T) to be $(D n)-dimensioanl static array.
+Gets the element type of the innermost array in a multidimensional static array type. 
+Considers $(D T) to be an $(D n)-dimensional static array type.
 
 Example:
 ---
@@ -4428,8 +4428,8 @@ unittest
 
 
 /**
-Get total multidimensional static array elements count considering 
-$(D T) to be $(D n)-dimensioanl static array.
+Calculates the total element count of a multidimensional static array.
+Considers $(D T) to be an $(D n)-dimensional static array type.
 
 Example:
 ---
